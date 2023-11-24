c127e7393313ca20f822ed96408d7ed431d4ef6f
Joao Ghabriel <joaoghabriel02@gmail.com>
2023-11-24 14:20:28 -0300

    initial setup

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        dark_orange: "#D87D4A",
+        light_orange: "#fbaf85",
+        light_gray_200: "#F1F1F1",
+        light_gray_100: "#FAFAFA",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
